53bacd2fd499186a4f76a96ab65ca9b8dbae8635
Lucas Lucena <lucas.ferlucena@gmail.com>
2019-04-17 01:13:31 -0300

    distance

--- validation_scan.py
+++ validation_scan.py
@@ -1,28 +1,23 @@
 # -*- coding: cp1252 -*-
-import pyrebase
-
 import blescan
 import sys
 
 import bluetooth._bluetooth as bluez
 
-config = {
-  }
+from firebase import firebase
 
-firebase = pyrebase.initialize_app(config)
-#### FORMACAO DA LISTA DE ENDEREÇOS MAC HABILITADOS ####
-db = firebase.database()
-users = db.child("users").get()
+firebase = firebase.FirebaseApplication('https://teste2-943e6.firebaseio.com/')
 
 knownaddress = []
-for usr in users.each():
-    knownaddress.append(usr.val())
 
-for x in knownaddress:
-    print(x)
+result = firebase.get('/users',None)
+for x in range(1, len(result)):
+	knownaddress.append(firebase.get('/users',str(x)))
 
-#### FIM ####
-#################################################################
+print ("Endereços cadastrados:")
+
+for y in knownaddress:
+	print (y)
 
 
 dev_id = 0
@@ -39,14 +34,21 @@ blescan.hci_enable_le_scan(sock)
 
 while True:
 	returnedList = blescan.parse_events(sock, 10)
-	print "----------------------------------------"
-	for beacon in returnedList:
-            ##separar o beacon em partes para comparar somente o enderço mac
-            adqaddress = beacon
-            if adq_address in knownaddress: 
-                knownaddress.index(adq_address)
-                print ("ENTRADA PERMITIDA")
-            else:
-                print ("ENTRADA NEGADA")
+	print "----------"
+	scanAddress = []
+	for x in returnedList:
+		#print(x)
+		add = str(x).split(",")
+		if int(add[5]) >= -70:		
+			scanAddress.append(add[0])
 
+	scanAddress = list(dict.fromkeys(scanAddress))
+
+	for beacon in scanAddress:
+		if beacon in knownaddress:
+			print("LIBERADO", beacon)
+		else:
+			print("ACESSO NEGADO", beacon)
+		
+		
 ##definir um numero de vezes que o scan será executado e quando a lista será atualizada
